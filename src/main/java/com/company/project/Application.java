9215874df2bbb96b3346aee83c83b2c345977de7
myscs <1282858658@qq.com>
2020-04-30 22:47:04 +0800

    prd --dev

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -2,21 +2,20 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args)  {
         SpringApplication.run(Application.class, args);
     }
 
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+//
+//     extends SpringBootServletInitializer
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
 }
 
